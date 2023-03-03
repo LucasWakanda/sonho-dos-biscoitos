c035e9e39ed094366ad8c2665a7b62e2d2f8a827
Lucas Wakanda <lucascorreiaaraujo77@gmail.com>
2023-03-03 01:29:08 -0300

    implementa api de teste

--- src/main/java/br/com/sonhodosbiscoitos/cadastrocomercio/CadastroComercioApplication.java
+++ src/main/java/br/com/sonhodosbiscoitos/cadastrocomercio/CadastroComercioApplication.java
@@ -2,12 +2,21 @@ package br.com.sonhodosbiscoitos.cadastrocomercio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class CadastroComercioApplication {
-
+  
+	@GetMapping 
+	public String getHomeTeste() {
+		return "Cadastro comercio";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroComercioApplication.class, args);
 	}
-
 }
