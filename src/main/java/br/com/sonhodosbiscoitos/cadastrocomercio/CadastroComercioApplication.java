2d1cb65836a251f25ab06598427f663097201c8b
Lucas Wakanda <lucascorreiaaraujo77@gmail.com>
2023-03-03 00:38:20 -0300

    inicia projeto

---
+++ src/main/java/br/com/sonhodosbiscoitos/cadastrocomercio/CadastroComercioApplication.java
@@ -0,0 +1,13 @@
+package br.com.sonhodosbiscoitos.cadastrocomercio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroComercioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroComercioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sonhodosbiscoitos/cadastrocomercio/CadastroComercioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sonhodosbiscoitos.cadastrocomercio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroComercioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
