90e59d6f56f9965681de63b74e7ccb4dfcd628db
Lucas Wakanda <lucascorreiaaraujo77@gmail.com>
2023-03-08 21:22:47 -0300

    implementa controller

--- src/main/java/br/com/sonhodosbiscoitos/clienteempresa/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/sonhodosbiscoitos/clienteempresa/cliente/application/api/ClienteAPI.java
@@ -1,5 +1,18 @@
 package br.com.sonhodosbiscoitos.clienteempresa.cliente.application.api;
 
-public interface ClienteAPI {
+import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/cliente")
+public interface ClienteAPI {
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+ClienteResponse postCliente (@Valid @RequestBody ClienteRequest clienteRequest);
 }
